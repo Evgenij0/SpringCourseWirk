d209317c916f86c16452cabaaecf53aab58dac71
Aliterego <sotnikovvlad01@gmail.com>
2024-03-09 10:44:48 +0400

    My firts commit! My firts commit! My firts commit! My firts commit!

--- src/main/java/com/cours/work/demo/controller/ClientController.java
+++ src/main/java/com/cours/work/demo/controller/ClientController.java
@@ -25,7 +25,7 @@ public class ClientController {
         clientServies.save(Client);
 
     }
-//комментарий
+//комментарий!
     @GetMapping()
     public List<Client> getAllClients() {
         return clientServies.getAll();
